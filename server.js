2f8a0e9154a39d0d5f58da503f8752d0b0781089
Parinita Salagala <parinitasalagala@gmail.com>
2025-10-29 00:39:36 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
+
 require("dotenv").config();
 
 app.set("view engine", "ejs");
@@ -9,17 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const API_KEY =process.env.API;
 
-
 app.get("/", (req, res) => {
     const london = {
-    city: "London",
-    temp: 18,
-    condition: "Cloudy",
-    humidity: 65,
-    icon: "https://cdn.weatherapi.com/weather/64x64/day/116.png"
+     city: "London",
+     temp: 18,
+     condition: "Cloudy",
+     humidity: 65,
+     icon: "https://cdn.weatherapi.com/weather/64x64/day/116.png"
   };
   res.render("index", { weather:null, error: null });
 });
+
 
 app.post("/", async (req, res) => {
   const city = req.body.city;
@@ -30,15 +31,15 @@ app.post("/", async (req, res) => {
     const data = response.data;
 
     const weather = {
-      city: data.location.name,
-      temp: data.current.temp_c,
-      condition: data.current.condition.text,
-      icon: data.current.condition.icon,
-      humidity: data.current.humidity,
+      city:data.location.name,
+      temp:data.current.temp_c,
+      condition:data.current.condition.text,
+      icon:data.current.condition.icon,
+      humidity:data.current.humidity,
     };
-
     res.render("index", { weather, error: null });
-  } catch (err) {
+  } 
+  catch (err) {
     res.render("index", { weather: null, error: "City not found!" });
   }
 });
